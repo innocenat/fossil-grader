bfa0a204b710cc694e8166103565c1a4a36ec3f4
jittat <jittat@c6549ec2-22b3-11de-91f4-d7b858dd2e09>
2012-04-30 09:02:15 +0000

    added more test for memory limits

---
+++ grader/trunk/src/test/submissions/add/addmemfail1a.c
@@ -0,0 +1,19 @@
+/*
+LANG: C
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+  int a,b;
+  int *buf = malloc(17000000);
+  a = 10;
+  b = 20;
+  int i;
+  for(i=0; i<100000000; i++)
+    a += b;
+  scanf("%d %d",&a,&b);
+  printf("%d\n",a+b);
+  free(buf);
+}
